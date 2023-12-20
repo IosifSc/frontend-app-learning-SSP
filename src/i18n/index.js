e6cfa6334c81b32f7dd17ae37f099198435967ea
IosifSc <iosif.taviscridon@gmail.com>
2023-12-20 12:13:17 +0000

    Añadir traducciones a los Idiomas de SSP

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,34 +2,31 @@ import { messages as footerMessages } from '@edx/frontend-component-footer';
 import { messages as headerMessages } from '@edx/frontend-component-header';
 import { messages as paragonMessages } from '@edx/paragon';
 
-import arMessages from './messages/ar.json';
+import arMessages from './messages/no_usadas_ssp/ar.json';
 import frMessages from './messages/fr.json';
-import es419Messages from './messages/es_419.json';
+import es419Messages from './messages/no_usadas_ssp/es_419.json';
 import zhcnMessages from './messages/zh_CN.json';
-import ptMessages from './messages/pt.json';
-import itMessages from './messages/it.json';
-import ukMessages from './messages/uk.json';
-import deMessages from './messages/de.json';
+import ukMessages from './messages/no_usadas_ssp/uk.json';
 import ruMessages from './messages/ru.json';
-import hiMessages from './messages/hi.json';
-import faIRMessages from './messages/fa_IR.json';
-import frCAMessages from './messages/fr_CA.json';
+import hiMessages from './messages/no_usadas_ssp/hi.json';
+import faIRMessages from './messages/no_usadas_ssp/fa_IR.json';
+import frCAMessages from './messages/no_usadas_ssp/fr_CA.json';
 import dedeCAMessages from './messages/de_DE.json';
 import ititCAMessages from './messages/it_IT.json';
 import ptptCAMessages from './messages/pt_PT.json';
 // no need to import en messages-- they are in the defaultMessage field
 import esesMessages from './messages/es-es.json';
 import thMessages from './messages/th.json';
+import plMessages from './messages/pl.json';
+import koKRMessages from './messages/ko_KR.json';
+import nbMessages from './messages/nb.json';
+import nlNLMessages from './messages/nl_NL.json';
 
 const appMessages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
-  pt: ptMessages,
-  it: itMessages,
-  de: deMessages,
-  hi: hiMessages,
   'fa-ir': faIRMessages,
   'fr-ca': frCAMessages,
   ru: ruMessages,
@@ -39,6 +36,10 @@ const appMessages = {
   'pt-pt': ptptCAMessages,
   'es-es': esesMessages,
   'th': thMessages,
+  pl: plMessages,
+  'ko-kr': koKRMessages,
+  'nb': nbMessages,
+  'nl-nl': nlNLMessages,
 };
 
 export default [
